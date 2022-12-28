007655d1a31731911d047e0441c0efb1dba70811
Pedro Silva <pedrosilvacwb@gmail.com>
2022-12-28 09:09:57 -0300

    feat:add feat navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
+const theme = createTheme({});
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );
